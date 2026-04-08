300964b9f264835197380a985ffb327b777d4232
ede-mzv <garbyedenilson@gmail.com>
2026-04-08 16:00:00 -0600

    chore: add database seeding and final configuration

---
+++ order-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO customers (first_name, last_name, email, phone, password)
+VALUES ('Edenilson', 'Demo', 'demo@test.com', '+503 7000-0000',
+        '$2a$10$N9qo8uLOickgx2ZMRZoMyeIjZAgcfl7p92ldGxad68LJZdL17lhWy');
