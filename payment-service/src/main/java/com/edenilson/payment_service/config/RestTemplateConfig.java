1722b4652120ced7d6829533bc9b2b64db0c32a3
ede-mzv <garbyedenilson@gmail.com>
2026-04-09 00:45:00 -0600

    fix: correct RestTemplateConfig import and defer data.sql initialization

--- payment-service/src/main/java/com/edenilson/payment_service/config/RestTemplateConfig.java
+++ payment-service/src/main/java/com/edenilson/payment_service/config/RestTemplateConfig.java
@@ -4,7 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
